5a9f97e7f5053b2a76cc4d4074f6cfbe357077f3
ohayama4z <45777685+ohyama4z@users.noreply.github.com>
2021-02-25 23:41:05 +0900

    配布テーマをちょっとカスタマイズしたい

---
+++ blog/.vuepress/components/PostDisqus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="disqus_thread"></div>
+</template>
+
+<script>
+export default {
+  name: "PostDisqus",
+
+  mounted() {
+    var disqus_config = function() {
+      this.page.url = window.location.origin;
+      this.page.identifier = window.location.pathname;
+    };
+    (function() {
+      // DON'T EDIT BELOW THIS LINE
+      var d = document,
+        s = d.createElement("script");
+      s.src = "https://ohyama4z-blog.netlify.app.disqus.com/embed.js";
+      s.setAttribute("data-timestamp", +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
+  },
+};
+</script>
