c3346b37273ba78c75b74940a571cccf0de008fd
ohayama4z <45777685+ohyama4z@users.noreply.github.com>
2021-02-26 19:23:36 +0900

    Disqusでコメントを実装

--- blog/.vuepress/theme/components/PostDisqus.vue
+++ blog/.vuepress/theme/components/PostDisqus.vue
@@ -5,7 +5,6 @@
 <script>
 export default {
   name: "PostDisqus",
-
   mounted() {
     var disqus_config = function() {
       this.page.url = window.location.origin;
@@ -15,7 +14,7 @@ export default {
       // DON'T EDIT BELOW THIS LINE
       var d = document,
         s = d.createElement("script");
-      s.src = "https://ohyama4z-blog.netlify.app.disqus.com/embed.js";
+      s.src = "https://oy4z-blog.disqus.com/embed.js";
       s.setAttribute("data-timestamp", +new Date());
       (d.head || d.body).appendChild(s);
     })();
--- blog/.vuepress/theme/layouts/Post.vue
+++ blog/.vuepress/theme/layouts/Post.vue
@@ -3,53 +3,48 @@
     <PostMeta v-if="meta" />
 
     <article class="main-div">
-      <Content
-        :key="$page.path"
-        class="post-content content"
-      />
+      <Content :key="$page.path" class="post-content content" />
     </article>
 
     <PostMeta v-if="meta" />
-
-    <div
-      v-if="vssue"
-      id="post-comments"
-      class="main-div"
-    >
-      <Vssue
-        :title="vssueTitle"
-        :issue-id="vssueId"
-      />
-    </div>
+    <Post-disqus />
   </div>
 </template>
 
 <script>
-import PostMeta from '@theme/components/PostMeta.vue'
+import PostMeta from "@theme/components/PostMeta.vue";
+import PostDisqus from "../components/PostDisqus";
 
 export default {
-  name: 'Post',
+  name: "Post",
 
   components: {
     PostMeta,
+    PostDisqus,
   },
 
   computed: {
-    meta () {
-      return this.$frontmatter.meta !== false
+    meta() {
+      return this.$frontmatter.meta !== false;
     },
 
-    vssue () {
-      return this.$themeConfig.comments !== false && this.$frontmatter.vssue !== false && (this.vssueTitle || this.vssueId)
+    vssue() {
+      return (
+        this.$themeConfig.comments !== false &&
+        this.$frontmatter.vssue !== false &&
+        (this.vssueTitle || this.vssueId)
+      );
     },
 
-    vssueTitle () {
-      return this.$frontmatter['vssue-title'] || this.$frontmatter.title || undefined
+    vssueTitle() {
+      return (
+        this.$frontmatter["vssue-title"] || this.$frontmatter.title || undefined
+      );
     },
 
-    vssueId () {
-      return this.$frontmatter['vssue-id'] || undefined
+    vssueId() {
+      return this.$frontmatter["vssue-id"] || undefined;
     },
   },
-}
+};
 </script>
